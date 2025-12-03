00b21a4c26fbd9a18eff2ba3ba2e4f64aa809eea
Kevin Zoltany <kzoltany@gmail.com>
2025-12-02 22:57:17 -0800

    Using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod ui;
 
-use gtk4::glib;
 use ui::AmplitudeApplication;
 
-fn main() -> glib::ExitCode {
-    spdlog::default_logger().set_level_filter(spdlog::LevelFilter::All);
-
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = AmplitudeApplication::new();
-    app.expect("An Error Occured").run()
-}
+    app.expect("An Error Occured").run();
 
+    Ok(())
+}
