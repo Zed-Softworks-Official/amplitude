b762dc5b436e76bb908e4684635813561a70aae9
Kevin Zoltany <kzoltany@gmail.com>
2025-12-02 19:28:08 -0800

    Super basic GTK window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("dev.zedsoftworks.amplitude")
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(1280)
+            .default_height(720)
+            .title("Amplitude")
+            .build();
+
+        let button = Button::with_label("Click Me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.set_child(Some(&button));
+
+        window.present();
+    });
+
+    app.run()
 }
