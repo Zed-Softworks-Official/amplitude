c46f101350237ce9f300446e86078189bc10cd58
Kevin Zoltany <kzoltany@gmail.com>
2026-02-23 19:44:59 -0800

    Temp fix for wayland

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // TODO: Find a fix for this on wayland
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+
     amplitude_lib::run()
 }
